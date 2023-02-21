27f73b7a8e27e1c7730436a59bac0581fa693a3e
“Robert <robert_venters@gap.com>
2023-02-21 13:04:28 -0800

    Update gnome-settings.sh

---
+++ gnome-settings.sh
@@ -0,0 +1,11 @@
+# View current settings - gsettings list-recursively org.gnome.desktop.interface
+
+# Clock Settings
+gsettings set org.gnome.desktop.interface clock-format '12h'
+
+# Enable window buttons
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
+# Set new windows centered
+gsettings set org.gnome.mutter center-new-windows true
+
